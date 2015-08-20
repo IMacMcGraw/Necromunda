49be0e7893ae1e07a1ff7c515efa2c2a3ce48ee1
Ian <ianmccall93@gmail.com>
2015-08-20 14:28:28 -0700

    Added 'necromunda.dart' and 'launcher.html': the app's entry point.

---
+++ source/necromunda.dart
@@ -0,0 +1,23 @@
+
+library necromunda;
+//import 'dart:html' as html;
+//import 'dart:math' as math;
+//import 'dart:async' as async;
+
+
+main(){
+
+//	var document = html.window.document;
+//	var clickStream = document.onClick;
+//	asyncCall(clickStream);
+//	print('Doesn\'t freeze up on me!!!!');
+
+}
+
+//asyncCall(async.Stream clickStream) async{
+//	var i = 0;
+//	await for(html.MouseEvent click in clickStream){
+//		print(click.page);
+//		if(click.page == new math.Point(0,0)) return;
+//	}
+//}
